ae6b2b3dcbc14dfb6e61acf6d29a048df441603c
daelflodo <davidflodo@gmail.com>
2023-09-01 21:11:38 -0500

    deploy 3

--- src/app.module.ts
+++ src/app.module.ts
@@ -16,15 +16,25 @@ config();
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.DB_PORT),
-      username: process.env.DB_USERNAME,
-      password: process.env.DB_PASSWORD,
-      database: process.env.DB_DATABASE,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true,
+      type: 'postgres',
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  entities: ['dist/**/*.entity{.ts,.js}'],
+  synchronize: true,
     }),
+    // TypeOrmModule.forRoot({
+    //   type: 'mysql',
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   entities: ['dist/**/*.entity{.ts,.js}'],
+    //   synchronize: true,
+    // }),
     ConfigModule.forRoot({
       envFilePath: Enviroments[process.env.NODE_ENV] || 'env',
       isGlobal: true,
